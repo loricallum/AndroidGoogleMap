a8a65d777c4a87abfb711d432fc64c884089697a
LLewis <lorimlewis@gmail.com>
2018-12-26 15:18:59 +0000

    Add files via upload

---
+++ MapsActivity.java
@@ -0,0 +1,43 @@
+package com.llewis.lorilewis.mapapp;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker and move the camera
+        LatLng northampton = new LatLng(52, -0.9);
+        mMap.addMarker(new MarkerOptions()
+                .position(northampton)
+                .title("Northampton, UK")
+                .snippet("Population: 215,173"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(northampton));
+
+    }
+
+    }
